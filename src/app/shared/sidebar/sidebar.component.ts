c64093098ccefe7f60e7a95b904850e750138ea6
JoseFranciscoPeraltaRamos <f.peralta@dappertechnologies.com>
2022-04-17 22:25:52 -0500

    Terminado

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
 
-  constructor() { }
+export class SidebarComponent{
 
-  ngOnInit(): void {
+  get obtenerHistorial(){
+    return this.gifsService.historial
+  }
+  constructor(private gifsService:GifsService){}
   }
 
-}
+
